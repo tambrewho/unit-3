a12e6b45892ab2320efe8bb08084fc96ea4a69a9
Jakob Hildebrand <jacobhildebrand58@yahoo.com>
2020-03-11 16:35:40 -0500

    created bubble chart of example city data

--- js/main.js
+++ js/main.js
@@ -24,4 +24,47 @@ window.onload = function(){
       .attr("x", 50) //position from left on the x (horizontal) axis
       .attr("y", 50) //position from top on the y (vertical) axis
       .style("fill", "#FFFFFF"); //fill color
+
+      var cityPop = [
+        {
+            city: 'Madison',
+            population: 233209
+        },
+        {
+            city: 'Milwaukee',
+            population: 594833
+        },
+        {
+            city: 'Green Bay',
+            population: 104057
+        },
+        {
+            city: 'Superior',
+            population: 27244
+        }
+    ];
+
+    var circles = container.selectAll(".circles") //create an empty selection
+        .data(cityPop) //here we feed in an array
+        .enter() //one of the great mysteries of the universe
+        .append("circle") //inspect the HTML--holy crap, there's some circles there
+        .attr("class", "circles")
+        .attr("id", function(d){
+            return d.city;
+        })
+        .attr("r", function(d){
+            //calculate the radius based on population value as circle area
+            var area = d.population * 0.01;
+            return Math.sqrt(area/Math.PI);
+        })
+        .attr("cx", function(d, i){
+            //use the index to place each circle horizontally
+            return 90 + (i * 180);
+        })
+        .attr("cy", function(d){
+            //subtract value from 450 to "grow" circles up from the bottom instead of down from the top of the SVG
+            return 450 - (d.population * 0.0005);
+        });
+
+
 };
