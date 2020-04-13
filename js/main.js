6634b53c3f8dcfb218423b23f9d8780ad6390f7f
Tambre Hu <thu.1215@gmail.com>
2020-04-13 07:23:08 -0400

    A10: L1 complete
    added color scale

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+//pseudo-global variables
+var attrArray = ["$500 and under", "over $500",	"Public Peace Violation",	"Motor Vehicle Theft", "Financial Theft", "Domestic"]; //list of attributes
+var expressed = attrArray[0]; //initial attribute
+
 //begin script when window loads
 window.onload = setMap();
 
@@ -16,10 +20,10 @@ function setMap(){
 
     // projection to make map cartographically accurate
     var projection = d3.geoAlbers()
-        .center([0, 41.83])
+        .center([-0.01, 41.83])
         .rotate([87.65, 0, 0])
         .parallels([29, 45])
-        .scale(80000.00)//extra zoom since this is a large scale map
+        .scale(97000.00)//extra zoom since this is a large scale map
         .translate([width / 2, height / 2]);
 
     var path = d3.geoPath()
@@ -28,7 +32,7 @@ function setMap(){
     //use Promise.all to parallelize asynchronous data loading
     var promises = [];
     promises.push(d3.csv("data/chicago_crimes.csv")); //load attributes from csv
-    promises.push(d3.json("data/chicago_areas.topojson")); //load background spatial data
+    promises.push(d3.json("data/Chicago_Neighborhoods.topojson")); //load background spatial data
     promises.push(d3.json("data/states.topojson")); // load choropleth spatial data
     Promise.all(promises).then(callback);
 
@@ -42,11 +46,10 @@ function setMap(){
 
         //topojson is used to be brought into the datum when using d3
         //chicagoNeighborhoods has .features at the end so it can map out all the nieghborhood boundaries
-        var chicagoNeighborhoods = topojson.feature(areas, areas.objects.Chicago_Neighborhoods).features;
+        var chicagoCrimes = topojson.feature(areas, areas.objects.Chicago_Neighborhoods).features;
         var stateRegions = topojson.feature(state, state.objects.states);
 
-        console.log(chicagoNeighborhoods);
-        console.log(stateRegions);
+        chicagoCrimes = joinData(chicagoCrimes, csvData);
 
         //midwest variable brings in the Illinois and Indiana state boundarie
         var regions = map.append("path")
@@ -58,14 +61,91 @@ function setMap(){
 
         //.community is different as the map will select all from the chicagoNeighborhoods data and then brings it in.
         var chi = map.selectAll(".neighborhood")
-            .data(chicagoNeighborhoods)
+            .data(chicagoCrimes)
             .enter()
             .append("path")
             //draws the boundaries of each neighborhood
             .attr("class", function(d){
-                return "neighborhood " + d.properties.Neighborho;
+                return "neighborhood " + d.properties.Neighborhood;
             })
             .attr("d", path);
-        };
 
+        //create color scale
+        var colorScale = makeColorScale(csvData);
+
+        //set enumeration units function is called
+        setEnumerationUnits(chicagoCrimes, map, path, colorScale);
+
+        };
+};
+
+function joinData(chicagoCrimes, csvData){
+  //loop through csv to assign each set of csv attribute values to geojson region
+  for (var i=0; i<csvData.length; i++){
+      var csvRegion = csvData[i]; //the current region
+      var csvKey = csvRegion.Neighborhood; //the CSV primary key
+
+      //loop through geojson regions to find correct region
+      for (var a=0; a<chicagoCrimes.length; a++){
+          var geojsonProps = chicagoCrimes[a].properties; //the current region geojson properties
+          var geojsonKey = geojsonProps.Neighborhood; //the geojson primary key
+
+          //where primary keys match, transfer csv data to geojson properties object
+          if (geojsonKey == csvKey){
+
+              //assign all attributes and values
+              attrArray.forEach(function(attr){
+                  var val = parseFloat(csvRegion[attr]); //get csv attribute value
+                  geojsonProps[attr] = val; //assign attribute and value to geojson properties
+              });
+          };
+      };
+  };
+
+    return chicagoCrimes;
+};
+
+//function to create color scale generator
+function makeColorScale(data){
+    var colorClasses = [
+        "#D4B9DA",
+        "#C994C7",
+        "#DF65B0",
+        "#DD1C77",
+        "#980043"
+    ];
+
+    //create color scale generator
+    var colorScale = d3.scaleQuantile()
+        .range(colorClasses);
+
+    //build two-value array of minimum and maximum expressed attribute values
+    var minmax = [
+        d3.min(data, function(d) { return parseFloat(d[expressed]); }),
+        d3.max(data, function(d) { return parseFloat(d[expressed]); })
+    ];
+    //assign two-value array as scale domain
+    colorScale.domain(minmax);
+
+    return colorScale;
+};
+
+function setEnumerationUnits(chicagoCrimes, map, path, colorScale){
+
+    var regions = map.selectAll(".community")
+        .data(chicagoCrimes)
+        .enter()
+        .append("path")
+        .attr("class", function(d){
+            return "regions " + d.properties.Neighborhood;
+        })
+        .attr("d", path)
+        .style("fill", function(d){
+            var value = d.properties[expressed];
+            if(value) {
+            	return colorScale(d.properties[expressed]);
+            } else {
+            	return "#ccc";
+            }
+    });
 };
